edf6b4decd268f108658d4c105caeeec35ce41a8
Hubert Rozmarynowski <hrozmarynowski776@gmail.com>
2022-12-27 15:18:21 +0100

    Show current balances for the current user on the main page

--- lib/employee_reward_web/controllers/employee_controller.ex
+++ lib/employee_reward_web/controllers/employee_controller.ex
@@ -4,12 +4,28 @@ defmodule EmployeeRewardWeb.EmployeeController do
   alias EmployeeReward.Repo
   alias EmployeeReward.Accounts.Employee
 
-  def index(conn, _params) do
-    employees = Repo.all(Employee)
-    render(conn, "index.html", employees: employees)
+  def action(conn, _) do
+    apply(__MODULE__, action_name(conn), [conn, conn.params, conn.assigns.current_employee])
   end
 
-  def show(conn, params) do
+  def index(conn, _params, current_employee) do
+     %{id: current_employee_id} = current_employee
+
+    loaded_employee =
+      Employee
+      |> Repo.get!(current_employee_id)
+      |>Repo.preload(:points_balance)
+
+    points = %{
+      to_grant: loaded_employee.points_balance.points_to_grant,
+      obtained: loaded_employee.points_balance.points_obtained
+    }
+
+    employees = Repo.all(Employee)
+    render(conn, "index.html", employees: employees, points: points)
+  end
+
+  def show(conn, params, _current_employee) do
     %{"id" => id} = params
     employee = Repo.get!(Employee, id)
     render(conn, "show.html", employee: employee)
