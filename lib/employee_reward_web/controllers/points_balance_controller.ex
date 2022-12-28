eb21ff6a2272035ed14b22270fbfdc6447c2be6f
Hubert Rozmarynowski <hrozmarynowski776@gmail.com>
2022-12-28 23:03:20 +0100

    Add a case statement for parsing transaction value

--- lib/employee_reward_web/controllers/points_balance_controller.ex
+++ lib/employee_reward_web/controllers/points_balance_controller.ex
@@ -4,17 +4,23 @@ defmodule EmployeeRewardWeb.PointsBalanceController do
   alias EmployeeReward.Rewards
 
   def grant(conn, %{"to_id" => to_id, "value" => value}) do
-    from_id = conn.assigns.current_employee.id
-    to_id = String.to_integer(to_id)
-    value = String.to_integer(value)
+    case Integer.parse(value) do
+      {value, _} ->
+        from_id = conn.assigns.current_employee.id
+        to_id = String.to_integer(to_id)
 
-    case Rewards.grant_points(from_id, to_id, value) do
-      {:ok, _} ->
+        case Rewards.grant_points(from_id, to_id, value) do
+          {:ok, _} ->
+            conn
+            |> redirect(to: Routes.employee_path(conn, :index))
+          {:error, message} ->
+            conn
+            |> put_flash(:error, message)
+            |> redirect(to: Routes.employee_path(conn, :index))
+        end
+      :error ->
         conn
-        |> redirect(to: Routes.employee_path(conn, :index))
-      {:error, message} ->
-        conn
-        |> put_flash(:error, message)
+        |> put_flash(:error, "Provided value for points must be an integer")
         |> redirect(to: Routes.employee_path(conn, :index))
     end
   end
