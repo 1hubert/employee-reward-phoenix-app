3f91cae549eff3a1a27e035caf0ac3c877e027fa
Hubert Rozmarynowski <hrozmarynowski776@gmail.com>
2023-01-01 19:18:07 +0100

    Updates for Heroku deploy

--- config/prod.exs
+++ config/prod.exs
@@ -19,16 +19,26 @@ config :logger, level: :info
 # To get SSL working, you will need to add the `https` key
 # to the previous section and set your `:url` port to 443:
 #
-#     config :employee_reward, EmployeeRewardWeb.Endpoint,
-#       ...,
-#       url: [host: "example.com", port: 443],
-#       https: [
-#         ...,
-#         port: 443,
-#         cipher_suite: :strong,
-#         keyfile: System.get_env("SOME_APP_SSL_KEY_PATH"),
-#         certfile: System.get_env("SOME_APP_SSL_CERT_PATH")
-#       ]
+
+
+config :employee_reward, EmployeeRewardWeb.Endpoint,
+  http: [port: {:system, "PORT"}],
+  url: [scheme: "https", host: "arcane-temple-41768.herokuapp.com"],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]]
+
+
+# config :employee_reward, EmployeeRewardWeb.Endpoint,
+#   ...,
+#   url: [scheme: "https", host: "arcane-temple-41768.herokuapp.com", port: 443],
+#   force_ssl: [rewrite_on: [:x_forwarded_proto]],
+#   secret_key_base: System.get_env("SECRET_KEY_BASE")
+#   https: [
+#     ...,
+#     port: 443,
+#     cipher_suite: :strong,
+#     keyfile: System.get_env("SOME_APP_SSL_KEY_PATH"),
+#     certfile: System.get_env("SOME_APP_SSL_CERT_PATH")
+#   ]
 #
 # The `cipher_suite` is set to `:strong` to support only the
 # latest and more secure SSL ciphers. This means old browsers
--- config/runtime.exs
+++ config/runtime.exs
@@ -31,10 +31,10 @@ if config_env() == :prod do
   maybe_ipv6 = if System.get_env("ECTO_IPV6"), do: [:inet6], else: []
 
   config :employee_reward, EmployeeReward.Repo,
-    # ssl: true,
+    ssl: true,
     url: database_url,
     pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
-    socket_options: maybe_ipv6
+    #socket_options: maybe_ipv6
 
   # The secret key base is used to sign/encrypt cookies and other secrets.
   # A default value is used in config/dev.exs and config/test.exs but you
