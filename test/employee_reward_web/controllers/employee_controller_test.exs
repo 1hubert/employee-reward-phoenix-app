60d846fe8302b3d9f560d93347ee45d24a930460
Hubert Rozmarynowski <hrozmarynowski776@gmail.com>
2023-01-01 17:40:36 +0100

    Modify employee_controller_test.exs

--- test/employee_reward_web/controllers/employee_controller_test.exs
+++ test/employee_reward_web/controllers/employee_controller_test.exs
@@ -1,84 +1,17 @@
 defmodule EmployeeRewardWeb.EmployeeControllerTest do
   use EmployeeRewardWeb.ConnCase
 
-  import EmployeeReward.RolesFixtures
+  import EmployeeReward.AccountsFixtures
+  import EmployeeRewardWeb.ConnCase
 
-  @create_attrs %{email: "example@gmail.com", name: "John", surname: "Smith", points_obtained: 42, points_to_grant: 42, password: "12345"}
-  @update_attrs %{email: "example@protonmail.com", name: "David", surname: "Brown", points_obtained: 43, points_to_grant: 43, password: "password"}
-  @invalid_attrs %{email: nil, name: nil, password: nil, points_obtained: nil, points_to_grant: nil, surname: nil}
+  @create_attrs %{email: "example@gmail.com", name: "John", surname: "Smith", password: "12345"}
+  @update_attrs %{email: "example@protonmail.com", name: "David", surname: "Brown", password: "password"}
+  @invalid_attrs %{email: nil, name: nil, surname: nil, password: nil}
 
   describe "index" do
-    test "lists all employees", %{conn: conn} do
+    test "redirected to log in when not logged in", %{conn: conn} do
       conn = get(conn, Routes.employee_path(conn, :index))
-      assert html_response(conn, 200) =~ "Listing Employees"
+      assert redirected_to(conn) == Routes.employee_session_path(conn, :new)
     end
-  end
-
-  describe "new employee" do
-    test "renders form", %{conn: conn} do
-      conn = get(conn, Routes.employee_path(conn, :register))
-      assert html_response(conn, 200) =~ "Register"
-    end
-  end
-
-  describe "create employee" do
-    test "redirects to show when data is valid", %{conn: conn} do
-      conn = post(conn, Routes.employee_path(conn, :create), employee: @create_attrs)
-
-      assert %{id: id} = redirected_params(conn)
-      assert redirected_to(conn) == Routes.employee_path(conn, :show, id)
-
-      conn = get(conn, Routes.employee_path(conn, :show, id))
-      assert html_response(conn, 200) =~ "Show Employee"
-    end
-
-    test "renders errors when data is invalid", %{conn: conn} do
-      conn = post(conn, Routes.employee_path(conn, :create), employee: @invalid_attrs)
-      assert html_response(conn, 200) =~ "Register"
-    end
-  end
-
-  describe "edit employee" do
-    setup [:create_employee]
-
-    test "renders form for editing chosen employee", %{conn: conn, employee: employee} do
-      conn = get(conn, Routes.employee_path(conn, :edit, employee))
-      assert html_response(conn, 200) =~ "Edit Employee"
-    end
-  end
-
-  describe "update employee" do
-    setup [:create_employee]
-
-    test "redirects when data is valid", %{conn: conn, employee: employee} do
-      conn = put(conn, Routes.employee_path(conn, :update, employee), employee: @update_attrs)
-      assert redirected_to(conn) == Routes.employee_path(conn, :show, employee)
-
-      conn = get(conn, Routes.employee_path(conn, :show, employee))
-      assert html_response(conn, 200) =~ "example@protonmail.com"
-    end
-
-    test "renders errors when data is invalid", %{conn: conn, employee: employee} do
-      conn = put(conn, Routes.employee_path(conn, :update, employee), employee: @invalid_attrs)
-      assert html_response(conn, 200) =~ "Edit Employee"
-    end
-  end
-
-  describe "delete employee" do
-    setup [:create_employee]
-
-    test "deletes chosen employee", %{conn: conn, employee: employee} do
-      conn = delete(conn, Routes.employee_path(conn, :delete, employee))
-      assert redirected_to(conn) == Routes.employee_path(conn, :index)
-
-      assert_error_sent 404, fn ->
-        get(conn, Routes.employee_path(conn, :show, employee))
-      end
-    end
-  end
-
-  defp create_employee(_) do
-    employee = employee_fixture()
-    %{employee: employee}
   end
 end
